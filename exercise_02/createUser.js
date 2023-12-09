da572e7dfff8df0adbc5c601ee7a6a46dff5d07a
AdriaCParra <Costaadria524@gmail.com>
2023-12-09 11:44:58 +0100

    Finish exercise_02

--- exercise_02/createUser.js
+++ exercise_02/createUser.js
@@ -13,3 +13,13 @@
  * output
  *  - string
  */
+
+const createUser = (email, onError, onSuccess) => {
+  const randomNumber = Math.random();
+
+  if (randomNumber < 0.5 || email === "") return onError();
+
+  return onSuccess(email);
+};
+
+export default createUser;
---
+++ exercise_02/createUser.test.js
@@ -0,0 +1,44 @@
+import createUser from "./createUser";
+
+describe("createUser", () => {
+  it("should call onError callback if email is empty", () => {
+    const email = "";
+    const onError = jest.fn();
+    const onSuccess = jest.fn();
+
+    createUser(email, onError, onSuccess);
+
+    expect(onError).toHaveBeenCalled();
+    expect(onSuccess).not.toHaveBeenCalled();
+  });
+
+  it("should call onError callback if random number is less than 0.5", () => {
+    const email = "test@example.com";
+    const onError = jest.fn();
+    const onSuccess = jest.fn();
+
+    jest.spyOn(Math, "random").mockReturnValue(0.4);
+
+    createUser(email, onError, onSuccess);
+
+    expect(onError).toHaveBeenCalled();
+    expect(onSuccess).not.toHaveBeenCalled();
+
+    jest.spyOn(Math, "random").mockRestore();
+  });
+
+  it("should call onSuccess function if email is not empty and random number is greater than or equal to 0.5", () => {
+    const email = "test@example.com";
+    const onError = jest.fn();
+    const onSuccess = jest.fn();
+
+    jest.spyOn(Math, "random").mockReturnValue(0.6);
+
+    createUser(email, onError, onSuccess);
+
+    expect(onError).not.toHaveBeenCalled();
+    expect(onSuccess).toHaveBeenCalled();
+
+    jest.spyOn(Math, "random").mockRestore();
+  });
+});
